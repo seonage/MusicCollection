456ff0d6f97fca90d35ef03ab8c2c39c1769026c
seonage <steven.lin1986@gmail.com>
2017-06-29 17:40:13 -0400

    Forgot to commit some files that should have been in previous commit

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -2,5 +2,5 @@ var Album = require('../models/album');
 
 //GET the form that allows new albums to be created
 exports.createAlbumGet = function(req, res, next){
-	res.render('album_form', { title: 'Create album' });
+	res.render('album_create', { title: 'Create album' });
 };
