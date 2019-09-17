4fbb06722a0b2295990077a02743b76beecf8d79
Steven Lin <steven.lin1986@gmail.com>
2019-09-17 19:18:41 -0400

    Entering artistName and artistBio in the form update artistName and artistBiography in state

--- musiccollectionreact/src/Components/CreateArtist.js
+++ musiccollectionreact/src/Components/CreateArtist.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
 class CreateArtist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {artistName: '', artistBiography: ''};
+
+        this.handleArtistNameChange = this.handleArtistNameChange.bind(this);
+        this.handleArtistBiographyChange = this.handleArtistBiographyChange.bind(this);
+    }
+
+    handleArtistNameChange(event) {
+        this.setState({artistName: event.target.value});
+    }
+
+    handleArtistBiographyChange(event) {
+        this.setState({artistBiography: event.target.value});
+    }
+
     render() {
         return (
             <div>
                 <h3>Add New Artist</h3>
-                <form action="" method="POST">
+                <form action="/artist/create" method="POST">
                 <label>Artist Name
-                    <input type="text" name="artistName"/>
+                    <input type="text" name="artistName" value={this.state.artistName} onChange={this.handleArtistNameChange}/>
                 </label>
                 <br/>
                 <label>Biography
-                    <input type="text" name="artistBio"/>
+                    <input type="text" name="artistBio" value={this.state.artistBiography} onChange={this.handleArtistBiographyChange}/>
                 </label>
                 <br/>
                 <input type="submit" name="Submit"/>
