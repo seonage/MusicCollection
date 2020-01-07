42269f3c04ca8d2dea29876d5b778c362ae33550
Steven Lin <steven.lin1986@gmail.com>
2020-01-07 00:30:42 -0500

    Set Mongoose to use new URl string parser

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var users = require('./routes/users');
 const options = {
 		reconnectTries: Number.MAX_VALUE,
         poolSize: 10,
+        useNewUrlParser:true
 };
 
 var mongoDB = "mongodb://admin:<>@cluster0-shard-00-00-hsep4.mongodb.net:27017,cluster0-shard-00-01-hsep4.mongodb.net:27017,cluster0-shard-00-02-hsep4.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
