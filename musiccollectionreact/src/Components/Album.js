9d28e6570157f80fa21f7752ec5fed1f14b257c7
Steven Lin <steven.lin1986@gmail.com>
2020-10-01 22:06:45 -0400

    Albums can now be deleted from React end

--- musiccollectionreact/src/Components/Album.js
+++ musiccollectionreact/src/Components/Album.js
@@ -8,21 +8,39 @@ class Album extends Component{
             albumID: props.match.params.albumID,
             albumInfo: []
         }
+
+        this.deleteAlbum = this.deleteAlbum.bind(this);
 }
 
     componentDidMount() {
         let albumURI = '/album/' + this.state.albumID;
-        console.log(albumURI)
+        console.log("Comp did mount: " + albumURI)
+
         fetch(albumURI)
         .then(res => res.json())
         .then(body => this.setState({albumInfo: body}))
         .catch(error => console.log(error))
     }
 
+    deleteAlbum() {
+        let albumURI = '/album/' + this.state.albumID + '/delete';
+        console.log("Delete worked: " + albumURI);
+
+        fetch(albumURI, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(this.props.history.push('/'));
+    }
+
     render() {
         return(
             <div>
             <h2>{this.state.albumInfo.title}</h2>
+            <button onClick = {this.deleteAlbum}>Delete Album</button>
             <Link to= '/'>Return to list of artists</Link>
             </div>
         )
