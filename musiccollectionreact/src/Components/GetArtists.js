940c0f854f31948de8a5c73349cc1bf79dce57bd
Steven Lin <steven.lin1986@gmail.com>
2021-02-01 14:46:30 -0500

    Added search function for GetArtists component

--- musiccollectionreact/src/Components/GetArtists.js
+++ musiccollectionreact/src/Components/GetArtists.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 class GetArtists extends Component{
     constructor() {
         super();
+        
         this.state = {
-            artists: []
+            artists: [],
+            searchValue: ''
         }
+
+        this.artistFilterOnChange = this.artistFilterOnChange.bind(this);
     }
 
     componentDidMount() {
@@ -15,12 +19,27 @@ class GetArtists extends Component{
         .then(body => this.setState({artists: body}, console.log('Fetched: ', body)))
         .catch(error => console.log(error))
     }
+
+    artistFilterOnChange = (event) => {
+        console.log("Artist Filter: ", event.target.value)
+        this.setState({
+            searchValue: event.target.value
+        })
+    }
+
     render() {
+        const filteredArtists = this.state.artists.filter(artist => {
+            return artist.artist_name.includes(this.state.searchValue);
+        })
+        console.log("Filtered artists :" + filteredArtists.map(artist => artist.artist_name));
+
         return(
             <div>
             <h2>Artists</h2>
+            <label>Search For An Artist</label>
+                <input type="text" onChange={this.artistFilterOnChange} />
             <ul>
-                {this.state.artists.map(artist => 
+                {filteredArtists.map(artist => 
                     <li key={artist._id}><Link to= {'artist/' + artist._id}>{artist.artist_name}</Link></li>
                 )}
             </ul>
