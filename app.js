a17b35949dda067492e13f52458f32c0dfbb8a8c
Steven Lin <steven.lin1986@gmail.com>
2022-04-13 17:30:07 -0400

    Changed localhost addresses

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
