d47ef99eb17a2623c3be8f545a7d38b6cef5e111
seonage <steven.lin1986@gmail.com>
2024-01-12 16:14:59 -0500

    Remove outdated parts

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const {check, validationResult} = require('express-validator');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb');
 
 require('dotenv').config()
 const env = process.env.NODE_ENV;
